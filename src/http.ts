1b092876b367b0357ea17485063d78afb4cee061
Dykiert, Agata <agata.dykiert@atos.net>
2019-10-04 09:50:05 +0200

    add http methods

--- src/http.ts
+++ src/http.ts
@@ -1,1 +1,32 @@
-// export const getTodos = async () => {}
+var getTodos = async () => {
+    const response = await fetch('http://localhost:3000/todos')
+    return response.json()
+}
+
+var getTodo = async (id) => {
+    const response = await fetch('http://localhost:3000/todos/' + id)
+    return response.json()
+}
+
+var createTodo = async (data) => {
+    const response = await fetch('http://localhost:3000/todos', {
+        method: 'POST',
+        body: data
+    })
+    return response.json()
+}
+
+var deleteTodo = async (id) => {
+    const response = await fetch('http://localhost:3000/todos/' + id, {
+        method: 'DELETE'
+    })
+    return response.json()
+}
+
+var updateTodo = async (id, data) => {
+    const response = await fetch('http://localhost:3000/todos/' + id, {
+        method: 'PUT',
+        body: data
+    })
+    return response.json()
+}
